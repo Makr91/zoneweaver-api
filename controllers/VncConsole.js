f54625a5afc6bd67c19e23d8f2641b13cc380a56
Makr91 <mark.gilbert@m4kr.net>
2025-09-10 05:45:17 -0500

    feat: linting - v2

--- controllers/VncConsole.js
+++ controllers/VncConsole.js
@@ -837,7 +837,6 @@ export const startVncSession = async (req, res) => {
         const existingProcesses = lines.filter(line => line.includes('zadm') && 
                            (line.includes('vnc') || line.includes('webvnc')) &&
                            line.includes(zoneName));
-        });
 
         if (existingProcesses.length > 0) {
           log.websocket.info('Found unhealthy VNC processes - killing', {
